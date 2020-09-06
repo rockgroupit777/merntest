d409043839bfb32e4db0d799c976a19e17001a92
rockgroupit777 <rockgroupit777@gmail.com>
2020-09-06 21:40:03 +0700

    basic step

--- server/src/users/resolver.ts
+++ server/src/users/resolver.ts
@@ -2,8 +2,9 @@ import { gql, PubSub } from "apollo-server-express";
 import jwt from "jsonwebtoken";
 import config from "../config";
 import { fields, objectIdValidate } from "../utils";
-import { signInValidate, signUpValidate } from "./validate";
+import { signInValidate, signUpValidate, updateUserValidate } from "./validate";
 import { User, UserDocument } from "./";
+import { attemptSignIn } from "./middleware";
 
 const pubsub = new PubSub();
 const USER_ADDED = "USER_ADDED";
@@ -34,6 +35,10 @@ const userQueries = {
     }
   },
 };
+
+/**
+ * @ User Mutation
+ */
 const userMutation = {
   signUp: async (parent: any, args: { createUserInput: any }) => {
     try {
@@ -54,7 +59,57 @@ const userMutation = {
       throw error;
     }
   },
+  updateUser: async (
+    parent: any,
+    args: { userId: string; updateUserInput: any },
+    context: any
+  ) => {
+    if (!context.isAuth) {
+      throw new Error("Non Authenticated");
+    }
+    try {
+      await updateUserValidate.validateAsync(args.updateUserInput, {
+        abortEarly: false,
+      });
+      const user = await User.findByIdAndUpdate(
+        args.userId,
+        args.updateUserInput,
+        { new: true }
+      );
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  },
+  signIn: async (
+    parent: any,
+    args: { email: string; password: string },
+    req: { req: Request },
+    info: any
+  ) => {
+    try {
+      await signInValidate.validateAsync(args, { abortEarly: false });
+      const user = await attemptSignIn(args);
+      if (!user) {
+        throw new Error("User does not exist");
+      }
+      const token = jwt.sign({ userId: user._id }, config.jwtSecret, {
+        expiresIn: 3600,
+      });
+      return {
+        userId: user._id,
+        token,
+        tokenExpiration: 1,
+      };
+    } catch (error) {
+      throw error;
+    }
+  },
 };
-const userSubscription = {};
+const userSubscription = {
+  userAdded: {
+    subscribe: () => pubsub.asyncIterator([USER_ADDED]),
+  },
+};
 
 export { userQueries, userMutation, userSubscription };
