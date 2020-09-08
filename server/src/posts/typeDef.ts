653f28c11c9a9e990c5795bb0d7abcebd5b5b5a1
rockgroupit777 <rockgroupit777@gmail.com>
2020-09-08 17:13:31 +0700

    basic step

--- server/src/posts/typeDef.ts
+++ server/src/posts/typeDef.ts
@@ -4,4 +4,48 @@ export default gql`
     post(postId: ID!): Post!
     posts: [Post!]!
   }
+  type Mutation{
+      createPost(createPostInput:CreatePostInput): Post!
+      updatePost(postId:ID!,updatePostInput:UpdatePostInput): Post!
+  }
+  type Subcription{
+
+  }
+  type Post{
+    _id:ID!
+    title: String!
+    alias: String!
+    summary: String!
+    content: String!
+    cover: String
+    photos: [String]
+    user: [User!]
+    status: Boolean
+    likes: [User]
+    commentStatus: Boolean
+  }
+  input CreatePostInput{
+    title: String!
+    alias: String!
+    summary: String!
+    content: String!
+    cover: String
+    photos: [String]
+    user: [User!]
+    status: Boolean
+    likes: [User]
+    commentStatus: Boolean
+  }
+  input UpdatePostInput{
+    title: String!
+    alias: String!
+    summary: String!
+    content: String!
+    cover: String
+    photos: [String]
+    user: [User!]
+    status: Boolean
+    likes: [User]
+    commentStatus: Boolean
+  }
 `;
